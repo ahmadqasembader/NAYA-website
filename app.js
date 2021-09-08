f2624f42f2788bb3481dbda1b6e8147556ee373e
Ahmad Bader <67756902+ahmadqasembader@users.noreply.github.com>
2021-09-09 00:24:31 +0300

    re-model the gallary part 1

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const logo = document.querySelector(".logo");
 const header = document.querySelector(".header");
 const navbar = document.querySelector(".sticky_nav");
 const links = document.querySelectorAll(".nav_link");
-const navLinks = document.querySelector('.nav_links');
+const navLinks = document.querySelector(".nav_links");
 const numbers = document.querySelector(".numbers_container");
 const headerCoords = navbar.getBoundingClientRect();
 const scrollTopBtn = document.querySelector("#scrollTop");
 const slides = document.querySelectorAll(".my_slides");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const arrows = document.querySelectorAll('.arrows');
+const arrows = document.querySelectorAll(".arrows");
 
 const handleHover = function (e) {
   if (e.target.classList.contains("nav_link")) {
@@ -57,36 +57,36 @@ const headerObserver = new IntersectionObserver(stickyNav, navOptions);
 headerObserver.observe(header);
 
 // slider auto/manual navigation
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+// function plusSlides(n) {
+//   showSlides((slideIndex += n));
+// }
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+// function currentSlide(n) {
+//   showSlides((slideIndex = n));
+// }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("my_slides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("my_slides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " active";
+// }
 
 // animated number counter
 let initiative, people, project, trainHours;
@@ -192,11 +192,99 @@ document.addEventListener("keydown", function (e) {
 // Smooth scroll
 
 const scrollTo = (e) => {
-    e.preventDefault();
-    if(e.target.classList.contains('nav_link')){
-      const id = e.target.getAttribute('href');
-      document.querySelector(id).scrollIntoView({ behavior: 'smooth', block: 'center' });
-    }
-}
-navLinks.addEventListener('click', scrollTo);
+  e.preventDefault();
+  if (e.target.classList.contains("nav_link")) {
+    const id = e.target.getAttribute("href");
+    document
+      .querySelector(id)
+      .scrollIntoView({ behavior: "smooth", block: "center" });
+  }
+};
+navLinks.addEventListener("click", scrollTo);
 
+// Slideshow
+const slider = function () {
+  const dotContainer = document.querySelector(".dots");
+  const btnLeft = document.querySelector(".slider__btn--left");
+  const btnRight = document.querySelector(".slider__btn--right");
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  //create dots based on the number of slides inputted
+  const createDots = () => {
+    slides.forEach((_, i) => {
+      dotContainer.insertAdjacentHTML(
+        "before",
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  // active dot activation
+
+  const activeDot = function (slide) {
+    document
+      .querySelectorAll(".dots__dot")
+      .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active");
+  };
+
+  //Go to slide
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+      // (s, i) => (s.style.transform = `translateX(${100}%)`)
+    );
+  };
+
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+    activeDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activeDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+
+    activeDot(0);
+  };
+  init();
+
+  // Events Handler
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prevSlide();
+    e.key === "ArrowRight" && nextSlide();
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activeDot(slide);
+    }
+  });
+};
+slider();
